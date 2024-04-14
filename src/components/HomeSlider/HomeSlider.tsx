e28ce502dfd293ff9230fcb6351dd7cecfc63faf
Yitik Kawatra <yitikkawatra@gmai.com>
2024-04-14 16:49:09 +0530

    fix

--- src/components/HomeSlider/HomeSlider.tsx
+++ src/components/HomeSlider/HomeSlider.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState , useEffect} from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -11,11 +11,16 @@ import 'swiper/css/pagination';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 import Image from 'next/image';
 
-const width = globalThis.window.innerWidth;
-const height = globalThis.window.innerHeight
-
 
 const HomeSlider = () => {
+
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    useEffect(() => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }, []);
 
     const [banners, setBanners] = useState([
         {
